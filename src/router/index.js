f506e1d2578fd7649aac4306a6b71efd53f5e2f3
DESKTOP-NSUR2UR\GOTGIGA <treenate.got@gmail.com>
2026-01-10 18:31:53 +0700

    fix: change router history mode to hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router' 
 import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/home',
       name: 'home',
       component: HomeView,
-    },{
-      path:'/',
-      name:'login',
-      component:LoginView
+    },
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView
     }
-    
   ],
 })
 
